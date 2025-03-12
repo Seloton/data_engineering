e3b6f670c13645c0e23222a02d823a86cc641718
Seloton <dnlzhkv12@gmail.com>
2025-03-12 20:04:50 +1000

    sp500 is done

--- sp500.py
+++ sp500.py
@@ -28,34 +28,58 @@ def link_parser() -> list:
     return links
 
 
-def parser() -> dict:
+def parser() -> list:
     links = link_parser()
+    dollar = dollar_value()
     stock_date = []
     for stock_link in links:
-        page = requests.get(stock_link)
-        soup = BeautifulSoup(page.text, "lxml")
-        stock_dict = dict()
-        stock_dict["code"] = soup.find("span", "price-section__category").text.strip().split(" , ")[-1]
-        stock_dict["name"] = soup.find("span", "price-section__label").text
-        stock_dict["price"] = round(float(soup.find("span", "price-section__current-value").text) * dollar_value(), 2)
-        stock_dict["P/E"] = float(soup.find("div", "snapshot__data-item padding-right--zero").text.split()[0])
-        # stock_dict["growth"] = None  # Сделать!!!
-        week_lower = float(soup.find("div", "snapshot__data-item snapshot__data-item--small").text.split()[0])
-        week_higher = float(soup.find("div", "snapshot__data-item snapshot__data-item--small snapshot__data-item--right").text.split()[0])
-        stock_dict["potential profit"] = round((week_lower / week_higher) * 100, 2)  # Перевести в проценты и переписать чтобы брал за 52 недели
-        stock_date.append(stock_dict)
+        try:
+            page = requests.get(stock_link)
+            soup = BeautifulSoup(page.text, "lxml")
+            stock_dict = dict()
+            stock_dict["code"] = soup.find("span", "price-section__category").text.strip().split(" , ")[-1]
+            stock_dict["name"] = soup.find("span", "price-section__label").text
+            stock_dict["price"] = round(float(soup.find("span", "price-section__current-value").text.replace(',', '')) * dollar, 2)
+            stock_dict["P/E"] = float(soup.find("div", "snapshot__data-item padding-right--zero").text.split()[0].replace(',', ''))
+            week_lower = float(soup.find("div", "snapshot__data-item snapshot__data-item--small").text.split()[0].replace(',', ''))
+            week_higher = float(soup.find("div", "snapshot__data-item snapshot__data-item--small snapshot__data-item--right").text.split()[0].replace(',', ''))
+            stock_dict["potential profit"] = round(((week_higher - week_lower) / week_lower) * 100, 2)
+            stock_date.append(stock_dict)
+        except AttributeError:
+            print(f"Ошибка с: {stock_link}")
     return stock_date
 
 
-def create_json():
-    pass
+def most_expensive(stocks: list) -> list:
+    return sorted(stocks, key=lambda x: x['price'], reverse=True)[:10]
 
 
-def write_json():
-    pass
+def lowest_pe(stocks: list) -> list:
+    return sorted(stocks, key=lambda x: x['P/E'])[:10]
+
+
+def best_growth(stocks: list) -> list:
+    return sorted(stocks, key=lambda x: x['potential profit'], reverse=True)[:10]
+
+
+def create_json(stock: list) -> None:
+    with open("most_expensive.json", "w") as file:
+        json.dump(most_expensive(stock), file, indent=4)
+    with open("lowest_pe.json", "w") as file:
+        json.dump(lowest_pe(stock), file, indent=4)
+    with open("best_growth.json", "w") as file:
+        json.dump(best_growth(stock), file, indent=4)
+
 
 
 if __name__ == '__main__':
+    # print(*most_expensive(temp), sep='\n')
+    # print(*lowest_pe(temp), sep='\n')
+    # print(*best_growth(temp), sep='\n')
+
     # print(*parser(), sep='\n')
-    print(*link_parser(), sep='\n')
+    # print(*link_parser(), sep='\n')
     # print(dollar_value())
+
+    stock_date = parser()
+    create_json(stock_date)
