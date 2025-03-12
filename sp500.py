ae5c4ef3e2e757be5910dfa05e9256f7ae176b32
Seloton <dnlzhkv12@gmail.com>
2025-03-12 20:22:00 +1000

    Add new dir with json files

--- sp500.py
+++ sp500.py
@@ -1,3 +1,23 @@
+'''
+Homework
+Ваша задача спарсить информацию о компаниях, находящихся в индексе S&P 500 с данного сайта:
+https://markets.businessinsider.com/index/components/s&p_500
+
+Для каждой компании собрать следующую информацию:
+* Текущая стоимость в рублях (конвертацию производить по текущему курсу, взятому с сайта [центробанка РФ](http://www.cbr.ru/development/sxml/))
+* Код компании (справа от названия компании на странице компании)
+* P/E компании (информация находится справа от графика на странице компании)
+* Годовой рост/падение компании в процентах (основная таблица)
+* Высчитать какую прибыль принесли бы акции компании (в процентах), если бы они были куплены на уровне 52 Week Low и проданы на уровне 52 Week High (справа от графика на странице компании)
+
+Сохранить итоговую информацию в 4 JSON файла:
+1. Топ 10 компаний с самими дорогими акциями в рублях.
+2. Топ 10 компаний с самым низким показателем P/E.
+3. Топ 10 компаний, которые показали самый высокий рост за последний год
+4. Топ 10 компаний, которые принесли бы наибольшую прибыль, если бы были куплены на самом минимуме и проданы на самом максимуме за последний год.
+'''
+
+
 from bs4 import BeautifulSoup
 import requests
 import datetime
@@ -29,14 +49,15 @@ def link_parser() -> list:
 
 
 def parser() -> list:
+    # Парсим каждую страницу и записываем все в stock_date
     links = link_parser()
     dollar = dollar_value()
     stock_date = []
     for stock_link in links:
         try:
+            stock_dict = dict()
             page = requests.get(stock_link)
             soup = BeautifulSoup(page.text, "lxml")
-            stock_dict = dict()
             stock_dict["code"] = soup.find("span", "price-section__category").text.strip().split(" , ")[-1]
             stock_dict["name"] = soup.find("span", "price-section__label").text
             stock_dict["price"] = round(float(soup.find("span", "price-section__current-value").text.replace(',', '')) * dollar, 2)
@@ -51,35 +72,31 @@ def parser() -> list:
 
 
 def most_expensive(stocks: list) -> list:
+    # Топ 10 компаний с самими дорогими акциями в рублях
     return sorted(stocks, key=lambda x: x['price'], reverse=True)[:10]
 
 
 def lowest_pe(stocks: list) -> list:
+    # Топ 10 компаний с самым низким показателем P/E
     return sorted(stocks, key=lambda x: x['P/E'])[:10]
 
 
 def best_growth(stocks: list) -> list:
+    # Топ 10 компаний, которые принесли бы наибольшую прибыль, если бы были куплены на самом минимуме и проданы на самом максимуме за последний год.
     return sorted(stocks, key=lambda x: x['potential profit'], reverse=True)[:10]
 
 
 def create_json(stock: list) -> None:
-    with open("most_expensive.json", "w") as file:
+    # Записываем все в JSON файлы
+    with open("json/most_expensive.json", "w") as file:
         json.dump(most_expensive(stock), file, indent=4)
-    with open("lowest_pe.json", "w") as file:
+    with open("json/lowest_pe.json", "w") as file:
         json.dump(lowest_pe(stock), file, indent=4)
-    with open("best_growth.json", "w") as file:
+    with open("json/best_growth.json", "w") as file:
         json.dump(best_growth(stock), file, indent=4)
 
 
 
 if __name__ == '__main__':
-    # print(*most_expensive(temp), sep='\n')
-    # print(*lowest_pe(temp), sep='\n')
-    # print(*best_growth(temp), sep='\n')
-
-    # print(*parser(), sep='\n')
-    # print(*link_parser(), sep='\n')
-    # print(dollar_value())
-
     stock_date = parser()
     create_json(stock_date)
